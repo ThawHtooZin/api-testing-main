07846dd8154cc3e184d22275416297547b2ae4be
ThawHtooZin <thawhtooin@gmail.com>
2025-05-16 10:49:43 +0630

    Added student routes's functions to controller

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -2,9 +2,55 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Student;
 use Illuminate\Http\Request;
 
 class StudentController extends Controller
 {
-    //
+    /**
+     * Display a listing of the students.
+     */
+    public function index()
+    {
+        $students = Student::all();
+        return response()->json($students);
+    }
+
+    /**
+     * Display the specified student.
+     */
+    public function show($id)
+    {
+        $student = Student::findOrFail($id);
+        return response()->json($student);
+    }
+
+    /**
+     * Store a newly created student in storage.
+     */
+    public function store(Request $request)
+    {
+        $student = Student::create($request->all());
+        return response()->json($student);
+    }
+
+    /**
+     * Update the specified student in storage.
+     */
+    public function update(Request $request, $id)
+    {
+        $student = Student::findOrFail($id);
+        $student->update($request->all());
+        return response()->json($student);
+    }
+
+    /**
+     * Remove the specified student from storage.
+     */
+    public function destroy($id)
+    {
+        $student = Student::findOrFail($id);
+        $student->delete();
+        return response()->json(['message' => 'Student deleted successfully']);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,40 +1,22 @@
 <?php
 
-use App\Models\Student;
-use Illuminate\Http\Request;
+use App\Http\Controllers\StudentController;
 use Illuminate\Support\Facades\Route;
 
 Route::group(['prefix' => 'student-crud'], function () {
     // Get all students
-    Route::get('/students', function () {
-        $students = Student::all();
-        return response()->json($students);
-    });
+    Route::get('/students', [StudentController::class, 'index']);
 
     // Get a specific student
-    Route::get('/students/{id}', function ($id) {
-        $student = Student::findOrFail($id);
-        return response()->json($student);
-    });
+    Route::get('/students/{id}', [StudentController::class, 'show']);
 
     // Create a new student
-    Route::post('/students', function (Request $request) {
-        $student = Student::create($request->all());
-        return response()->json($student);
-    });
+    Route::post('/students', [StudentController::class, 'store']);
 
     // Update a student
-    Route::put('/students/{id}', function (Request $request, $id) {
-        $student = Student::findOrFail($id);
-        $student->update($request->all());
-        return response()->json($student);
-    });
+    Route::put('/students/{id}', [StudentController::class, 'update']);
 
     // Delete a student
-    Route::delete('/students/{id}', function ($id) {
-        $student = Student::findOrFail($id);
-        $student->delete();
-        return response()->json(['message' => 'Student deleted successfully']);
-    });
+    Route::delete('/students/{id}', [StudentController::class, 'destroy']);
 });
 
